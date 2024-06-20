5783767cdeac761eb7fc9e465e754db7f40ebed9
Eliel Cortes <159460211+ElielC12@users.noreply.github.com>
2024-06-20 14:47:45 -0400

    Update second.py

--- second.py
+++ second.py
@@ -1,3 +1,8 @@
+## Hey I'm Eliel learning to code.
+## Practice Problem From https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+## Create a program that asks the user to enter their name and their age. Print out a message addressed to them that
+## tells them the year that they will turn 100 years old.
+
 import datetime 
 #variable for current year
 current_year = datetime.datetime.now().year
