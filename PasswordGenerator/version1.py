03bde695a1bba3d3abbb69e6d4585ce53049587d
Eliel Cortes <159460211+ElielC12@users.noreply.github.com>
2024-06-20 18:37:04 -0400

    Add files via upload

--- PasswordGenerator/version1.py
+++ PasswordGenerator/version1.py
@@ -1,26 +1,36 @@
 from faker import Faker
 import string, random
 
+# Initialize the Faker library
 fake = Faker()
 
 print('Welcome to Password Generator')
-print('''Select your password strenght: 
+print('''Select your password strength: 
                  1. Weak
                  2. Strong ''')
-#Selects the strenght of the password
-strenght = int(input('Pick a number: '))
+
+# Select the strength of the password
+strength = int(input('Pick a number: '))
 password = []
-#This option generates two random words and appends to a list
-if strenght == 1:
+
+# If the user selects '1' for a weak password
+if strength == 1:
+    # Generate two random words using Faker
     word1 = fake.word()
     word2 = fake.word()
+    # Concatenate the two words and add them to the password list
     password.append(word1 + word2)
-#This option uses ascii letters, digits and punctuation
-elif strenght == 2:
+
+# If the user selects '2' for a strong password
+elif strength == 2:
+    # Create a list of characters including letters, digits, and punctuation
     characterList = string.ascii_letters + string.digits + string.punctuation
-    #Loops trough characterList and appends to password
-    for i in range(12): 
+    # Loop 12 times to generate a strong password with 12 random characters
+    for i in range(12):
+        # Choose a random character from the character list
         randomchar = random.choice(characterList)
+        # Add the random character to the password list
         password.append(randomchar)
-#Prints the password      
+
+# Print the generated password by joining the list into a single string
 print(f"Your new password is " + "".join(password))
